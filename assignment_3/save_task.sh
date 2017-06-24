d4d33d4e469d3079891b4b5a578504e1e4532edb
nyu-python <alice.r.topping@gmail.com>
2017-06-24 04:10:19 +0000

    adding task script

---
+++ assignment_3/save_task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+newTask="$1"
+echo" $newTask" >> ~/task_database.txt
+
+newTask="$1"
+
+# double quotes are expanded, single are not
+echo $newTask
+echo "$newTask"
+echo '$newTask'
+
+echo "------------"
+echo "first arg: $1"
+echo "============"
+echo "first arg: $2"
+echo "============"
+echo "first arg: $3"
